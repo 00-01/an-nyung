554252f130dbafcb4d2834e7d8d126b6046896ad
AnDeoukKyi <abdgf3@naver.com>
2021-06-17 11:02:20 +0900

    전체화면으로 변경 face_recognition 이식완료 테스트완료

--- face/mill_main.py
+++ face/mill_main.py
@@ -1,8 +1,11 @@
 import datetime
 import json
+import multiprocessing
 import os
 import platform
 import sys
+import time
+from multiprocessing import Process, Queue
 
 import cv2
 import face_recognition as fr
@@ -24,9 +27,8 @@ class layout_main(QMainWindow):
         self.layout = QtWidgets.QGridLayout()
         self.setCentralWidget(self.window)
         self.window.setLayout(self.layout)
-
         self.layout.setContentsMargins(0, 0, 0, 0)
-
+        # self.showFullScreen()#전체화면
         # 처음시작
         self.layout.addWidget(layout_start(self), 0, 0)
 
@@ -46,12 +48,6 @@ class layout_main(QMainWindow):
         self.run = False
         self.image = None
 
-        self.init()
-
-    def init(self):
-        # config json file read
-        with open('config.json') as json_file:
-            setting = json.load(json_file)
 
         # ---------------------------------------camera setting---------------------------------------------
         for s_os in setting["camera"]["os"]:
@@ -69,7 +65,6 @@ class layout_main(QMainWindow):
         self.viewROI = setting["view"]["roi"]
         self.viewFPS = setting["view"]["fps"]
 
-        pass
 
     def move_start(self):
         self.layout.addWidget(layout_start(self), 0, 0)
@@ -108,9 +103,9 @@ class layout_capture(QWidget):
         self.btn_save.clicked.connect(self.click_save)
         self.btn_back.clicked.connect(self.click_back)
 
-        self.take_photo = Take_photo()
-        self.take_photo.start()
-        self.take_photo.ImageUpdate.connect(self.ImageUpdateSlot)
+        self.take_Capture = takeCapture()
+        self.take_Capture.start()
+        self.take_Capture.ImageUpdate.connect(self.ImageUpdateSlot)
 
     def click_save(self):
         if self.et_name.text() == "":
@@ -122,7 +117,7 @@ class layout_capture(QWidget):
             if x == QMessageBox.Yes:
                 return
 
-        self.take_photo.terminate()
+        self.take_Capture.terminate()
         # self.imgLabel.pixmap().save("tmp.jpg")
 
         msg = QMessageBox()
@@ -134,16 +129,20 @@ class layout_capture(QWidget):
         frame2 = cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB)
         face_crop = fr.face_locations(frame2)
 
-        if len(face_crop) != 1:
-            print("인식못했거나 여러개 인식됨")
-            self.take_photo.start()
+        # 에러메세지 출력해야됨------------------------------------------
+        if len(face_crop) == 0:
+            print("인식못함")
+            self.take_Capture.start()
+            return
+        elif len(face_crop) > 1:
+            print("여러개 인식됨")
+            self.take_Capture.start()
             return
         face_crop = face_crop[0]
         tmp1 = np.copy(frame1)
         cv2.rectangle(tmp1,
                       (face_crop[3], face_crop[0]),
                       (face_crop[1], face_crop[2]),
-                      # (100, 100), (300, 300),
                       (0, 0, 255), 2)
 
         # msg.setIconPixmap(self.imgLabel.pixmap())
@@ -182,19 +181,19 @@ class layout_capture(QWidget):
         else:
             pass
         # capture restart
-        self.take_photo.start()
+        self.take_Capture.start()
 
-    def click_back(self):
-        self.take_photo.stop()
-        self.deleteLater()
-        self.parent.move_start()
 
     def ImageUpdateSlot(self, Image):
         self.imgLabel.setPixmap(QPixmap.fromImage(Image))
         self.imgLabel.setScaledContents(True)
 
+    def click_back(self):
+        self.take_Capture.terminate()
+        self.deleteLater()
+        self.parent.move_start()
 
-class Take_photo(QThread):
+class takeCapture(QThread):
     ImageUpdate = pyqtSignal(QImage)
 
     def run(self):
@@ -215,11 +214,87 @@ class layout_recognition(QWidget):
         loadUi('layout/recognition.ui', self)
         self.parent = parent
 
+        image_anlyizeActive = True
+        self.take_Recognition = takeRecognition()
+        self.take_Recognition.start()
+        self.take_Recognition.ImageUpdate.connect(self.ImageUpdateSlot)
+
+
+
+
         self.btn_back.clicked.connect(self.click_back)
 
+
+
+
+
+    def ImageUpdateSlot(self, Image):
+        self.imgLabel.setPixmap(QPixmap.fromImage(Image))
+        self.imgLabel.setScaledContents(True)
+
     def click_back(self):
+        self.take_Recognition.terminate()
         self.deleteLater()
         self.parent.move_start()
+
+
+class takeRecognition(QThread):
+    ImageUpdate = pyqtSignal(QImage)
+
+    def run(self):
+        self.RecognitionActive = True
+
+        cap_size = (setting["camera"]["roi"]["width"], setting["camera"]["roi"]["height"])
+        s = time.time()
+        while self.RecognitionActive:
+            ret, frame = cap.read()
+            frame = cv2.flip(frame, 1)
+            # cpu 코어 갯수 - 2개 만큼만 face_recognition돌림
+            # capture돌아가는 1개
+            # 여유분 1개
+            if q.qsize() < multiprocessing.cpu_count() - 2:
+                seconds = time.time() - s
+                if seconds > float(1) / (multiprocessing.cpu_count() - 2):
+                    s = time.time()
+                    # ROI
+                    # 중앙에서 cap_size만큼
+                    q.put(frame[int(frame.shape[1] / 2 - cap_size[1] / 2): int(frame.shape[1] / 2 + cap_size[1] / 2),
+                          int(frame.shape[0] / 2 - cap_size[0] / 2): int(frame.shape[0] / 2 + cap_size[0] / 2)])
+
+            fps = 0
+            try:
+                pass
+                # calculate fps
+                fps = 1 / seconds
+                fps = ("%.2f" % fps)
+            # print(f"fps : {fps}", '\n')
+            except:
+                pass
+
+            # ROI
+            if setting["view"]["roi"]:
+                cv2.imshow('roi',
+                           frame[int(frame.shape[0] / 2 - cap_size[0] / 2): int(frame.shape[0] / 2 + cap_size[0] / 2),
+                           int(frame.shape[1] / 2 - cap_size[1] / 2): int(frame.shape[1] / 2 + cap_size[1] / 2)])
+
+            # ROI사각형
+            cv2.rectangle(frame,
+                          (int(frame.shape[1] / 2 - cap_size[1] / 2), int(frame.shape[0] / 2 - cap_size[0] / 2)),
+                          (int(frame.shape[1] / 2 + cap_size[1] / 2), int(frame.shape[0] / 2 + cap_size[0] / 2)),
+                          (0, 0, 255), 2)
+            # fps출력
+            if setting["view"]["fps"]:
+                cv2.putText(frame, "fps : " + str(fps), (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            img = QImage(frame.data, frame.shape[1], frame.shape[0], QImage.Format_RGB888)
+            self.ImageUpdate.emit(img)
+
+
+
+
+
 
 
 
@@ -233,9 +308,42 @@ for s_os in setting["camera"]["os"]:
         break
 
 cap = cv2.VideoCapture(cam_num, cam_cap)
+q = Queue()
+pro_list = []
 
+_, _, col, error_col = access_db()
+n = list(col.find({}))
+names = [i['name'] for i in n]
+id = [j['id'] for j in n]
+
+
+
+
+
+def image_anlyize(q):
+
+    while True:
+        # 큐에 있는 frame가져와서 face_location처리
+        if q:
+            frame = q.get()
+            face_locations = fr.face_locations(frame)
+            if face_locations:
+                face_encodings = fr.face_encodings(frame, face_locations)
+
+                for fe in face_encodings:
+                    name = "- - -"
+                    face_distances = fr.face_distance(id, fe)
+                    best_match_index = np.argmin(face_distances)
+                    if face_distances[best_match_index] < setting["distance"]:
+                        name = names[best_match_index]
+                    print(name)
 
 if __name__ == '__main__':
+    for _ in range(multiprocessing.cpu_count() - 1):
+        p = Process(target=image_anlyize, args=(q,))
+        pro_list.append(p)
+        p.start()
+        time.sleep(float(1) / (multiprocessing.cpu_count() - 2))
     app = QApplication(sys.argv)
     myWindow = layout_main()
 
