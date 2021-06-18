4d8d5bc501be106ded8815421ef969804934d7bd
AnDeoukKyi <abdgf3@naver.com>
2021-06-18 09:34:04 +0900

    change ui qt5 > tkinter1

--- face/mill_faceRecognition.py
+++ face/mill_faceRecognition.py
@@ -1,5 +1,4 @@
-
-
+import threading
 
 import cv2
 import tkinter as tk
@@ -38,18 +37,69 @@ class layout_start(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
 
+        self.background = ImageTk.PhotoImage(file="layout/background.jpg")
+        tk.Label(self, image=self.background).pack(side="top")
 
-        self.imgtk = ImageTk.PhotoImage(file="layout/background.png")
-
-        tk.Label(self, image=self.imgtk).pack(side="top")
-
-
-
-        #tk.Label(self, text="Start page", font=('Helvetica', 18, "bold")).pack(side="top", fill="x", pady=5)
         tk.Button(self, text="Go to page layout_faceCapture",
-                  command=lambda: master.switch_frame(layout_faceCapture)).pack()
+                  command=lambda: master.switch_frame(layout_faceCapture)).pack(side="bottom", expand=True, fill="both")
         tk.Button(self, text="Go to page layout_faceRecognition",
-                  command=lambda: master.switch_frame(layout_faceRecognition)).pack()
+                  command=lambda: master.switch_frame(layout_faceRecognition)).pack(side="bottom", expand=True, fill="both")
+
+
+class layout_faceCapture(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        self.master = master
+        self.flag = False
+        self.flag = True
+        #camera
+        self.camera = tk.Label(self)
+        self.camera.pack(side="top")
+
+
+        self.frame = tk.Frame(self)
+        self.frame.pack(side="bottom")
+
+        tk.Button(self.frame, text="Save",
+                  command=self.click_save).pack(side="left")
+        self.name = tk.Entry(self.frame)
+        self.name.pack(side="left")
+        tk.Button(self.frame, text="Back",
+                  command=self.click_back).pack(side="right")
+        self.captureThread = threading.Thread(target=preview_th, args=(lambda:self.flag, self))
+        self.captureThread.daemon = True
+        self.captureThread.start()
+
+
+    def click_save(self):
+        #이미지가져옴>쓰레드 종료>메세지박스>확인,취소>쓰레드재시작
+        self.camera
+        self.flag = False
+        
+
+
+
+    def click_back(self):
+        self.flag = False
+        self.master.switch_frame(layout_start)
+
+    def change_img(self, img):  # 레이블의 이미지 변경
+        try:
+            img = cv2.flip(img, 1)
+            img = cv2.resize(img, (640, 400))
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            img = Image.fromarray(img)
+            self.src = ImageTk.PhotoImage(image=img)
+            self.camera['image'] = self.src
+        except:
+            pass
+
+def preview_th(stop, app):
+    while stop:
+        ret, frame = cam.read()
+        if ret:
+            app.change_img(frame)
+        cv2.waitKey(100)
 
 
 class layout_faceRecognition(tk.Frame):
@@ -60,13 +110,6 @@ class layout_faceRecognition(tk.Frame):
         tk.Button(self, text="Go back to start page",
                   command=lambda: master.switch_frame(layout_start)).pack()
 
-class layout_faceCapture(tk.Frame):
-    def __init__(self, master):
-        tk.Frame.__init__(self, master)
-        tk.Frame.configure(self,bg='red')
-        tk.Label(self, text="Page two", font=('Helvetica', 18, "bold")).pack(side="top", fill="x", pady=5)
-        tk.Button(self, text="Go back to start page",
-                  command=lambda: master.switch_frame(layout_start)).pack()
 
 
 
@@ -74,6 +117,7 @@ class layout_faceCapture(tk.Frame):
 
 
 
+cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 
 if __name__ == "__main__":
