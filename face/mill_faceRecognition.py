1c43fec1cadb0717aa6e5658272a7ed4cb85b6d4
AnDeoukKyi <abdgf3@naver.com>
2021-06-21 10:45:53 +0900

    multiprocess 자동종료추가 빈도 높은 결과값 출력

--- face/mill_faceRecognition.py
+++ face/mill_faceRecognition.py
@@ -7,7 +7,7 @@ import threading
 import time
 import tkinter as tk
 from multiprocessing import Queue, Process
-
+from datetime import datetime
 import cv2
 import face_recognition as fr
 import numpy as np
@@ -196,15 +196,13 @@ class ThreadCapture():
         while self.flag:
             ret, frame = cam.read()
             if ret:
-                img = cv2.flip(img, 1)
-                app.change_img(frame)
+                img = cv2.flip(frame, 1)
+                app.change_img(img)
             cv2.waitKey(10)
 
     def terminate(self):
         self.flag = False
 
-
-# 아직 이식안함
 class layout_faceRecognition(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
@@ -247,8 +245,25 @@ class ThreadRecognition():
 
     def run(self, app):
         self.flag = True
-        s = time.time()
+
+        for process in pro_list:
+            process.kill()
+
+        for _ in range(multiprocessing.cpu_count() - 1):
+            p = Process(target=image_anlyize, args=(q,result,))
+            pro_list.append(p)
+            p.start()
+
+        start_time = time.time()
+        delay_time = time.time()
         while self.flag:
+            # 얼굴 인식 멀티 프로세스가 덜열려있는지 체크해서 계속 숫자 맞춰줌
+            if len(pro_list) < multiprocessing.cpu_count():
+                p = Process(target=image_anlyize, args=(q,result))
+                pro_list.append(p)
+                p.start()
+                
+                
             ret, frame = cam.read()
             if ret:
                 frame = cv2.flip(frame, 1)
@@ -256,9 +271,9 @@ class ThreadRecognition():
                 # capture돌아가는 1개
                 # 여유분 1개
                 if q.qsize() < multiprocessing.cpu_count() - 2:
-                    seconds = time.time() - s
-                    if seconds > float(1) / (multiprocessing.cpu_count() - 2):
-                        s = time.time()
+                    dif_time = time.time() - start_time
+                    if dif_time > float(1) / (multiprocessing.cpu_count() - 2):
+                        start_time = time.time()
                         # ROI
                         # 중앙에서 cap_size만큼
                         q.put(
@@ -269,7 +284,7 @@ class ThreadRecognition():
                 try:
                     pass
                     # calculate fps
-                    fps = 1 / seconds
+                    fps = 1 / dif_time
                     fps = ("%.2f" % fps)
                 # print(f"fps : {fps}", '\n')
                 except:
@@ -284,6 +299,9 @@ class ThreadRecognition():
 
                 # ROI사각형
                 cv2.rectangle(frame,
+
+
+
                               (int(frame.shape[1] / 2 - cap_size[1] / 2), int(frame.shape[0] / 2 - cap_size[0] / 2)),
                               (int(frame.shape[1] / 2 + cap_size[1] / 2), int(frame.shape[0] / 2 + cap_size[0] / 2)),
                               (0, 0, 255), 2)
@@ -295,10 +313,22 @@ class ThreadRecognition():
                 app.change_img(frame)
 
             cv2.waitKey(10)
+            if time.time() - delay_time > 1:
+                if result.qsize() > 3:
+                    ls = []
+                    print("=============================start")
+                    for q_result in range(result.qsize()):
+                        ls.append(result.get())
+                    print(ls)
+                    print("선택결과 : ", end="")
+                    print(max(ls, key=ls.count))
+                    print("=============================end")
+                    delay_time = time.time()
 
     def terminate(self):
         self.flag = False
-
+        for process in pro_list:
+            process.kill()
 
 _, _, col, error_col = access_db()
 n = list(col.find({}))
@@ -306,22 +336,31 @@ names = [i['name'] for i in n]
 id = [j['id'] for j in n]
 
 
-def image_anlyize(q):
+def image_anlyize(q, result):
+    # 멀티프로세스는 30초동안 인식된 얼굴 안들어오면 자동 종료됨
+    count = 0
     while True:
         # 큐에 있는 frame가져와서 face_location처리
-        if q:
+        if q.qsize() != 0:
             frame = q.get()
             face_locations = fr.face_locations(frame)
-            if face_locations:
+            if len(face_locations) == 1:
+                count = 0
                 face_encodings = fr.face_encodings(frame, face_locations)
-
                 for fe in face_encodings:
                     name = "- - -"
                     face_distances = fr.face_distance(id, fe)
                     best_match_index = np.argmin(face_distances)
                     if face_distances[best_match_index] < setting["distance"]:
                         name = names[best_match_index]
-                    print(name)
+                    result.put(name)
+                    # print(name)
+            elif len(face_locations) == 0:
+                count += 1
+                if count > 20:
+                    print('종료합니다.')
+                    exit()
+                time.sleep(0.5)
 
 
 with open('config.json') as json_file:
@@ -338,7 +377,9 @@ cam = cv2.VideoCapture(cam_num, cam_cap)
 cap_size = (setting["camera"]["roi"]["width"], setting["camera"]["roi"]["height"])
 
 q = Queue()
+result = Queue()
 pro_list = []
+
 
 if __name__ == "__main__":
     if setting["log"]:
@@ -349,11 +390,11 @@ if __name__ == "__main__":
     # os별로 명령어가 다름
     # os.system("killall -9 python3")
 
-    for _ in range(multiprocessing.cpu_count() - 1):
-        p = Process(target=image_anlyize, args=(q,))
-        pro_list.append(p)
-        p.start()
-        time.sleep(float(1) / (multiprocessing.cpu_count() - 2))
+    # for _ in range(multiprocessing.cpu_count() - 1):
+    #     p = Process(target=image_anlyize, args=(q,))
+    #     pro_list.append(p)
+    #     p.start()
+    #     time.sleep(float(1) / (multiprocessing.cpu_count() - 2))
 
     app = layout_controller()
     app.mainloop()
